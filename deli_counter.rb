583100fab4aa2847570b40133be122e3916c180b
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-25 05:55:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,19 +9,19 @@ function currentLine(NewLine) {
     var line = []
     if (NewLine.length === 0) 
       return "The line is currently empty."
-    } end {
-      for(var i = 0; i < NewLine.length; i++) {
+     end 
+      for(var i = 0; i < NewLine.length; i++ {
         line += (i + 1) + ". " + NewLine[i] + ", "
-      }
+      )
       line = line.slice(0, line.length-2)
       return "The line is currently: " + line
-    }
+    )
     //
 }
 function nowServing(x) {
-  if (x.length === 0) {
+  if (x.length === 0) 
     return "There is nobody waiting to be served!"
-  } end {
+  )
     var name = x[0];
     x.splice(0, 1);
     return "Currently serving " + name + ".";

96a215818ec3cd0202b7706cceca98c45008b321
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-25 06:00:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name){
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) 
     return "Currently serving " + katzDeliLine.shift() + ".";
-  } end {
+
       return "There is nobody waiting to be served!";
   }
 }
